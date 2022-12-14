934bdc60ec652d2e74c4a5df5a2d136546235d3d
Lord World <mr.pro100y.patsan@mail.ru>
2022-12-15 00:33:13 +0700

    3.1 done

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -1,116 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Q1
 {
     class Program
     {
+        private static readonly Random randomizer = new Random();
+        private const double selectionSet = 100000;
+        private const double sellingPrice = 35;
+        private const double turningBaseCharge = 5;
+        public const double turningExtraCharge = 3;
+        private const double grindingBaseCharge = 4;
+        public const double grindingExtraCharge = 2;
+        private const double materialPrice = 20;
+
+
         static void Main(string[] args)
         {
-            for (int index = 0; index < 100000; index++)
+            double totalCharge = 0;
+            double totalIncome = 0;
+            double failedDetails = 0;
+            for (int index = 0; index < selectionSet; index++)
             {
                 Detail detail = new Detail();
                 detail.Turning();
-                if (detail.TurningOutcome.Item2)
+                if (detail.IsTurningFailed)
                 {
-                    detail.Grinding();
+                    ++failedDetails;
+                    totalCharge += detail.Price;
+                    continue;
                 }
-                detail.Totalize();
-            }           
+                detail.Grinding();
+                if (detail.IsGrindingFailed)
+                {
+                    ++failedDetails;
+                    totalCharge += detail.Price;
+                    continue;
+                }
+                totalCharge += detail.Price;
+                totalIncome += sellingPrice;
+            }
+
+            Console.WriteLine($"На {selectionSet} деталей:\n" +
+                $"Затраты на производство: {totalCharge} дн.ед.\n" +
+                $"Доход от производства: {totalIncome} дн.ед.\n" +
+                $"Прибыль от производства: {totalIncome - totalCharge} дн.ед.\n" +
+                $"Процент успешных изделий: {Math.Round((selectionSet - failedDetails) / selectionSet * 100.0, 2)}%\n" +
+                $"Средняя прибыль с детали: {Math.Round((totalIncome - totalCharge) / (selectionSet - failedDetails), 2)} дн.ед.\n");
         }
 
-
-    }
-
-    class Detail
-    {
-        private const double turningBaseCharge = 5;
-        private const double turningExtraCharge = 3;
-        private const double grindingBaseCharge = 4;
-        private const double grindingExtraCharge = 2;
-        private const double materialPrice = 20;
-
-        public (bool, bool) TurningOutcome;
-        public double TurningPrice = turningBaseCharge;
-        public (bool, bool) GrindingOutcome = (false, false);
-        public double GrindingPrice = 0;
-        public bool TotalOutcome;
-        public double TotalPrice;
-
-        public Detail()
+        class Detail
         {
+            public bool IsTurningFailed;
+            public bool IsGrindingFailed;
+            public double Price;
 
-        }
+            public Detail()
+            {
+                Price = materialPrice;
+            }
 
-        public void Turning()
-        {
-            double turningOutcome = new Random().NextDouble();
-            switch (turningOutcome)
+            public void Turning()
             {
-                case double value when (value < 0.08):
-                    {
-                        TurningOutcome = (false, false);
-                    }
-                    break;
-                case double value when (value < 0.2):
-                    {
-                        TurningOutcome = (false, true);
-                        TurningPrice += turningExtraCharge;
-                    }
-                    break;
-                default:
-                    {
-                        TurningOutcome = (true, true);
-                        TurningPrice += turningExtraCharge;
-                    }
-                    break;
+                Price += turningBaseCharge;
+                double turningOutcome = randomizer.NextDouble();
+                if (turningOutcome < 0.08)
+                {
+                    IsTurningFailed = true;
+                    return;
+                }
+                if (turningOutcome < 0.2)
+                {
+                    Price += turningExtraCharge;
+                }
             }
-        }
 
-        public void Grinding()
-        {
-            double upperGrindingOutcome = new Random().NextDouble();
-            double lowerGrindingOutcome = new Random().NextDouble();
-            GrindingPrice = grindingBaseCharge;
-            switch (upperGrindingOutcome)
+            public void Grinding()
             {
-                case double value when (value < 0.03):
-                    {
-                        GrindingOutcome = (false, GrindingOutcome.Item2);
-                    }
-                    break;
-                default:
-                    {
-                        GrindingOutcome = (true, GrindingOutcome.Item2);
-                    }
-                    break;
-            }
-            switch (lowerGrindingOutcome)
-            {
-                case double value when (value < 0.06):
-                    {
-                        GrindingOutcome = (GrindingOutcome.Item1, false);
-                    }
-                    break;
-                default:
-                    {
-                        GrindingOutcome = (GrindingOutcome.Item1, true);
-                    }
-                    break;
-            }
-            if (GrindingOutcome != (true, true) || GrindingOutcome != (false, false))
-            {
-                GrindingPrice += grindingExtraCharge;
-            }
-        }
+                double upperGrindingOutcome = randomizer.NextDouble();
+                double lowerGrindingOutcome = randomizer.NextDouble();
+                Price += grindingBaseCharge;
 
-        public void Totalize()
-        {
-            TotalOutcome = (TurningOutcome.Item2 && GrindingOutcome.Item1) && (TurningOutcome.Item2 && GrindingOutcome.Item2);
-            TotalPrice = materialPrice + TurningPrice + GrindingPrice;
+                if (upperGrindingOutcome < 0.03 && lowerGrindingOutcome < 0.06)
+                {
+                    IsGrindingFailed = true;
+                    return;
+                }
+                if (upperGrindingOutcome < 0.03 || lowerGrindingOutcome < 0.06)
+                {
+                    Price += grindingExtraCharge;
+                }
+            }
         }
     }
 }
---
+++ Q2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        private const double l = 1.2;
+        private const double m = 2;
+        private const int selectionSet = 100;
+        private const int workHours = 8 * 60;
+
+        static void Main(string[] args)
+        {
+            double loadFactor = l / m;
+            double serveChance = 1 / (1 + loadFactor);
+            double denyChance = loadFactor / (1 + loadFactor);
+            double throughput = l * serveChance;
+
+            var cpmRandomizer = new Random();
+            var clRandomizer = new Random();
+            for (int dayIndex = 0; dayIndex < selectionSet; dayIndex++)
+            {
+                double callsPerMin = 2.4 * cpmRandomizer.NextDouble();
+                double callLenght = 4 * clRandomizer.NextDouble();
+                for (double minute = 0; minute < workHours; minute += callLenght)
+                {
+                    if (minute % l)
+                }
+            }
+        }
+    }
+}
